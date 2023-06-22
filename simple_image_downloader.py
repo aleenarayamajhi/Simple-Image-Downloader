1948e394d3df889977677b70071dffe1b2c37535
Aleena Rayamajhi <126793934+aleenarayamajhi@users.noreply.github.com>
2023-06-22 12:10:21 -0500

    Add files via upload

---
+++ simple_image_downloader.py
@@ -0,0 +1,4 @@
+from simple_image_download import simple_image_download as si
+response = si.simple_image_download
+
+response().download(keywords = "red apples, green apples", limit = 100) #downloads 100 images each for both keywords
